905e376eb540a476ad688043effe618a101830db
Tushar swarnkar <121808606+Tusharedith@users.noreply.github.com>
2025-06-25 19:42:31 +0530

    Update schema.sql

--- Sql/schema.sql
+++ Sql/schema.sql
@@ -1,1 +1,19 @@
+CREATE DATABASE IF NOT EXISTS blinkit;
+USE blinkit;
+
+-- Table schema for BlinkitSales
+CREATE TABLE IF NOT EXISTS BlinkitSales (
+  Item_Fat_Content VARCHAR(20),
+  Item_Identifier VARCHAR(20),
+  Item_Type VARCHAR(50),
+  Outlet_Establishment_Year INT,
+  Outlet_Identifier VARCHAR(20),
+  Outlet_Location_Type VARCHAR(20),
+  Outlet_Size VARCHAR(20),
+  Outlet_Type VARCHAR(30),
+  Item_Visibility FLOAT,
+  Item_Weight FLOAT,
+  Sales FLOAT,
+  Rating INT
+);
 
